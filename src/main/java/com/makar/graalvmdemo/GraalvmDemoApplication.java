9cad125c8e66fcd4701d5abbca012d96825d53fc
VasylMakar <vmakar@aviso.ca>
2024-08-26 16:20:37 +0300

    Initial commit

---
+++ src/main/java/com/makar/graalvmdemo/GraalvmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.makar.graalvmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraalvmDemoApplication.class, args);
+    }
+
+}
