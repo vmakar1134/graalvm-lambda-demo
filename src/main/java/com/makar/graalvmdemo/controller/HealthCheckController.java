67c71b3c5b33f877c1dca0c13e9c78354d9daa73
VasylMakar <vmakar@aviso.ca>
2024-09-08 15:40:59 +0300

    return response entity instead of Strig

--- src/main/java/com/makar/graalvmdemo/controller/HealthCheckController.java
+++ src/main/java/com/makar/graalvmdemo/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package com.makar.graalvmdemo.controller;
 
+import com.makar.graalvmdemo.model.HealthResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     @GetMapping
-    public String health() {
-        return "OK";
+    public ResponseEntity<HealthResponse> health() {
+        final HealthResponse healthResponse = new HealthResponse();
+        healthResponse.setStatus("UP");
+        return ResponseEntity.ok(healthResponse);
     }
 
 }
---
+++ src/main/java/com/makar/graalvmdemo/model/HealthResponse.java
@@ -0,0 +1,12 @@
+package com.makar.graalvmdemo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HealthResponse {
+
+    private String status;
+
+}
